1a3b5c8e287a82053bdd2a36c8cdb09fa34064da
Rhuan Santos <rhuankito@hotmail.com>
2026-02-04 11:24:03 -0300

    Add user registration form in index.php

---
+++ index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <title>Inserir Usuário</title>
+</head>
+<body>
+
+<h2>Cadastrar Usuário</h2>
+
+<form action="inserir.php" method="POST">
+    <label>Nome:</label><br>
+    <input type="text" name="nome" required><br><br>
+
+    <label>Email:</label><br>
+    <input type="email" name="email" required><br><br>
+
+    <button type="submit">Salvar</button>
+</form>
+
+</body>
+</html>
