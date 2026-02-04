6c41d8f528d2de4cc0ce9ed3debed17079972210
Rhuan Santos <rhuankito@hotmail.com>
2026-02-04 09:30:38 -0300

    Create conexao1.php

---
+++ Testes/conexao1.php
@@ -0,0 +1,11 @@
+<?php
+// conexão.php
+$hostname = " "; #IP Privado, Público ou Endpoint
+$bancodedados = "site1"; #Nome da sua base
+$usuario = "root"; #Usuário de acesso
+$senha = "suasenha"; #Senha do Banco de Dados
+
+// Criando a conexão
+$conexao = new mysqli($hostname, $usuario, $senha, $bancodedados);
+
+?>
